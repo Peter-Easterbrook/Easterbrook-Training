03717d747f958c5c513db30281b0f270c9bb8fac
Peter-Easterbrook <peter.easterbrook@gmail.com>
2022-05-07 13:44:33 +0200

    GSAP on info contactForm

--- gsap.js
+++ gsap.js
@@ -17,8 +17,8 @@ function paraAnimation(para) {
   paraTL.from(para, {
     ease: 'back.out(1.2)',
     scale: 0,
-    y: 100,
-    duration: 3,
+    y: 10,
+    duration: 1,
   });
   return paraTL;
 }
@@ -28,14 +28,40 @@ let main = gsap.timeline({
     trigger: '.bi-gem',
     start: 'top top',
     end: '=+360',
-    // markers: true,
+    markers: true,
     scrub: true,
   },
 });
 main
   .add(paraAnimation('#p1'))
-  .add(paraAnimation('#p2'), '<+=20%')
-  .add(paraAnimation('#p3'), '<+=20%')
-  .add(paraAnimation('#p4'), '<+=20%')
-  .add(paraAnimation('#p5'), '<+=20%')
-  .add(paraAnimation('#p6'), '<+=20%');
+  .add(paraAnimation('#p2'), '<+=40%')
+  .add(paraAnimation('#p3'), '<+=40%')
+  .add(paraAnimation('#p4'), '<+=40%')
+  .add(paraAnimation('#p5'), '<+=40%')
+  .add(paraAnimation('#p6'), '<+=40%');
+
+// gsap.from('#priceCard', {
+//   scrollTrigger: {
+//     trigger: '#uber',
+//     start: 'top top',
+//     end: '=+700',
+//     scrub: 1,
+//     markers: true,
+//   },
+//   stagger: { amount: 4 },
+//   scale: 0,
+//   x: -300,
+//   duration: 2,
+// });
+
+gsap.from('.formContainer', {
+  scrollTrigger: {
+    trigger: '#angebot',
+    start: 'top top',
+    end: '=+380',
+    // markers: true,
+    scrub: 1,
+  },
+  scale: 0,
+  duration: 1,
+});
