0ca3b825225e5477e8861bd6d7c10db41b0d61fe
Peter-Easterbrook <peter.easterbrook@beka-software.at>
2022-12-03 21:26:48 +0100

    Switch accordion icons to Bootstrap

--- accordion.js
+++ accordion.js
@@ -6,15 +6,14 @@ for (let i = 0; i < toggles.length; i++) {
   toggles[i].addEventListener('click', () => {
     if (parseInt(contentDiv[i].style.height) !== contentDiv[i].scrollHeight) {
       contentDiv[i].style.height = contentDiv[i].scrollHeight + 'px';
-
       toggles[i].style.color = '#fff';
-      icons[i].classList.remove('fa-arrow-alt-circle-down');
-      icons[i].classList.add('fa-arrow-alt-circle-up');
+      icons[i].classList.remove('bi-arrow-down-circle');
+      icons[i].classList.add('bi-arrow-up-circle');
     } else {
       contentDiv[i].style.height = '0px';
       toggles[i].style.color = '#fff';
-      icons[i].classList.remove('fa-arrow-alt-circle-up');
-      icons[i].classList.add('fa-arrow-alt-circle-down');
+      icons[i].classList.remove('bi-arrow-up-circle');
+      icons[i].classList.add('bi-arrow-down-circle');
 
       contentDiv[i].classList.remove('heightActive');
       contentDiv[i].classList.add('heightActive');
@@ -24,8 +23,8 @@ for (let i = 0; i < toggles.length; i++) {
       if (j !== i) {
         contentDiv[j].style.height = 0;
         toggles[j].style.color = '#fff';
-        icons[j].classList.remove('fa-arrow-alt-circle-up');
-        icons[j].classList.add('fa-arrow-alt-circle-down');
+        icons[j].classList.remove('bi-arrow-up-circle');
+        icons[j].classList.add('bi-arrow-down-circle');
       }
     }
   });
