c7acd982a8a888a4bb82f459ce9c4c5dd577c323
Peter-Easterbrook <peter.easterbrook@beka-software.at>
2023-01-07 13:07:05 +0100

    Finally fixed the accordion border issue

--- accordion.js
+++ accordion.js
@@ -6,13 +6,10 @@ for (let i = 0; i < toggles.length; i++) {
   toggles[i].addEventListener('click', () => {
     if (parseInt(contentDiv[i].style.height) !== contentDiv[i].scrollHeight) {
       contentDiv[i].style.height = contentDiv[i].scrollHeight + 'px';
-      contentDiv[i].style.border = '1px solid #fffff7';
-      toggles[i].style.color = '#fff';
       icons[i].classList.remove('bi-arrow-down-circle');
       icons[i].classList.add('bi-arrow-up-circle');
     } else {
       contentDiv[i].style.height = '0px';
-      toggles[i].style.color = '#fff';
       icons[i].classList.remove('bi-arrow-up-circle');
       icons[i].classList.add('bi-arrow-down-circle');
       contentDiv[i].classList.remove('heightActive');
@@ -22,7 +19,6 @@ for (let i = 0; i < toggles.length; i++) {
     for (let j = 0; j < contentDiv.length; j++) {
       if (j !== i) {
         contentDiv[j].style.height = 0;
-        toggles[j].style.color = '#fff';
         icons[j].classList.remove('bi-arrow-up-circle');
         icons[j].classList.add('bi-arrow-down-circle');
       }
